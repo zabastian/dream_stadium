653284639e56d5f234c14e90e023e3e9faa978a9
zabastian <qlgh10gh@gmail.com>
2025-08-20 14:07:20 +0900

    :sparkles: feature-stadium

---
+++ src/main/java/com/example/dream_stadium/owner/stadium/entity/Stadium.java
@@ -0,0 +1,32 @@
+package com.example.dream_stadium.owner.stadium.entity;
+
+import com.example.dream_stadium.common.user.entity.BaseEntity;
+import com.example.dream_stadium.common.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stadium extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stadium_id",unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @ManyToOne
+    private User user;
+
+    public Stadium(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+}
